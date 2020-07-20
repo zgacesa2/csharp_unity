a57321786208e0a6ec4fa7ee58e67c1f4009dc99
Zoran <zoran.gacesa2@gmail.com>
2020-07-20 22:40:39 +0200

    Create wasd.cs

---
+++ C#_unity_skripte/wasd.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class wasd : MonoBehaviour
+{
+    Rigidbody rb;
+    public int brzina;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.AddForce(Vector3.left * brzina);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            rb.AddForce(Vector3.right * brzina);
+        }
+        else if (Input.GetKey(KeyCode.W))
+        {
+            rb.AddForce(Vector3.forward * brzina);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            rb.AddForce(Vector3.back * brzina);
+        }
+    }
+}
