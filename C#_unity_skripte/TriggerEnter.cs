192ed032b8367ccb7008e5748bf6781ed17679bb
Zoran <zoran.gacesa2@gmail.com>
2020-07-21 19:00:11 +0200

    Create TriggerEnter.cs
    Zid ubija objekt, vrati objekt na pocetnu poziciju restartanjem scene

---
+++ C#_unity_skripte/TriggerEnter.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TriggerEnter : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "cube1")
+        {
+            Destroy(this.gameObject);
+            SceneManager.LoadScene("SampleScene");
+        }
+    }
+}
