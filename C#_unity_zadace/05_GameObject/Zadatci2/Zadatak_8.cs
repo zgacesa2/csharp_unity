c049a224a4c4a90fcee74a3f1aeb127dc21dc8ab
Zoran <zoran.gacesa2@gmail.com>
2020-07-23 22:51:05 +0200

    Create Zadatak_8.cs

---
+++ C#_unity_zadace/05_GameObject/Zadatci2/Zadatak_8.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+public class Zadatak_8 : MonoBehaviour
+{
+
+    public GameObject prefarban;
+    float sekunda;
+    public int brojac;
+
+    private void Update()
+    {
+        sekunda += Time.deltaTime;
+
+        if (sekunda >= 10)
+        {
+            brojac++;
+            Instantiate(prefarban, Vector3.zero, Quaternion.identity);
+        }
+    }
+}
