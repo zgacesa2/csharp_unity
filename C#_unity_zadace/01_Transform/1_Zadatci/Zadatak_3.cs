caf2488d11b55cae8f25ca268828632307589084
Zoran <zoran.gacesa2@gmail.com>
2020-07-23 16:52:39 +0200

    Create Zadatak_3.cs

---
+++ C#_unity_zadace/01_Transform/1_Zadatci/Zadatak_3.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Rotirajte kocku za javnu vrijednost po sve tri osi svake sekunde
+
+public class Zadatak_3 : MonoBehaviour
+{
+    public float sveRotation;
+
+    public void Update()
+    {
+        transform.Rotate(Vector3.one * Time.deltaTime * sveRotation);
+    }
+}
