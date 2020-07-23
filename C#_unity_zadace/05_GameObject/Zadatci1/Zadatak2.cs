e01286db84db26822a42f6257fda08ff17811b86
Zoran <zoran.gacesa2@gmail.com>
2020-07-23 18:06:40 +0200

    Create Zadatak2.cs

---
+++ C#_unity_zadace/05_GameObject/Zadatci1/Zadatak2.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//U updatu palite ili gasite game object ovisno o njegovom stanju konstantno u suprotno
+
+public class Zadatak2 : MonoBehaviour
+{
+    public GameObject predmet;
+
+    private void Update()
+    {
+        if(predmet.activeSelf == true)
+        {
+            predmet.SetActive(false);
+        }
+        else if (predmet.activeSelf == false)
+        {
+            predmet.SetActive(true);
+        }
+    }
+}
