1ad4b433711a31cdd1a8c09c43c0f70c68fea273
Zoran <zoran.gacesa2@gmail.com>
2020-07-23 18:01:52 +0200

    Create Zadatak7.cs

---
+++ C#_unity_zadace/01_Transform/5_Zadatci/Zadatak7.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PetSedam : MonoBehaviour
+{
+    public float nekaJavnaVrijednost;
+
+    private void Start()
+    {
+        int i = 0;
+        while (i < 20)
+        {
+            transform.localScale += Vector3.right;
+            transform.localScale -= Vector3.up;
+            transform.localScale -= Vector3.forward;
+            i++;
+        }
+        
+    }
+}
