9af633685d1317c6efff9b554db8a8ae4f12b8dc
Zoran <zoran.gacesa2@gmail.com>
2020-07-23 18:03:50 +0200

    Create Zadatak3.cs

---
+++ C#_unity_zadace/05_GameObject/Zadatci2/Zadatak3.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Kada kocka padne na sferu neka se uništi sfera
+
+public class Zadatak3 : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider coll)
+    {
+        Destroy(coll.gameObject);
+    }
+}
