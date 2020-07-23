0edf10756bf19fb14fd711767dfc2600ae44c0e2
Zoran <zoran.gacesa2@gmail.com>
2020-07-23 18:03:54 +0200

    Create Zadatak5.cs

---
+++ C#_unity_zadace/05_GameObject/Zadatci2/Zadatak5.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Kada kocka prođe kroz sferu (izlaz) neka se deaktivira skripta
+
+public class Zadatak5 : MonoBehaviour
+{
+    private void Update()
+    {
+        Debug.Log("Skripta je aktivna!");
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        this.enabled = false;
+    }
+}
