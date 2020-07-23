c22c8813bb40b177a668dc253411d54a3ce95138
Zoran <zoran.gacesa2@gmail.com>
2020-07-23 17:55:04 +0200

    Create Zadatak_7.cs

---
+++ C#_unity_zadace/01_Transform/1_Zadatci/Zadatak_7.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Neka se sfera miče za 0.1 po sve tri osi svaki frame
+
+public class Zadatak_7 : MonoBehaviour
+{
+    private void Update()
+    {
+        transform.position += new Vector3(0.1f, 0.1f, 0.1f);  
+    }
+}
