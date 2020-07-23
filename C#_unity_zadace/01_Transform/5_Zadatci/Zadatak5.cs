cc82ea477fd1107ed4fb343b60f3169dfc758f9b
Zoran <zoran.gacesa2@gmail.com>
2020-07-23 18:01:44 +0200

    Create Zadatak5.cs

---
+++ C#_unity_zadace/01_Transform/5_Zadatci/Zadatak5.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubePump : MonoBehaviour
+{
+    // transform - 5zadatci - 5zadatak Unesite neku javnu vrijednost, napravite for petlju u start metodi koja će služiti 
+    // da se u svakom ponavljanju kocka poveća po x osi dok god je uvijet zadovoljen
+
+    public int x;
+
+    private void Start()
+    {
+        if(x < 0)
+        {
+            x *= -1;
+        }
+        
+        for(int i = 1; x > i; i++ )
+        {
+            transform.localScale += new Vector3(1, 0, 0); 
+        }
+    }
+
+}
