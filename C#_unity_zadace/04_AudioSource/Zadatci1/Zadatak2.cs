f6b8e4488dd06cdfe6dc51bc8ae6e68dbb9c6de8
Zoran <zoran.gacesa2@gmail.com>
2020-07-23 18:06:07 +0200

    Create Zadatak2.cs

---
+++ C#_unity_zadace/04_AudioSource/Zadatci1/Zadatak2.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Napravite skriptu gdje jedan frame se pokreće zvuk,
+// a drugi frame stopira zvuk i tako u beskonacnost
+
+public class Zadatak2 : MonoBehaviour
+{
+    AudioSource ass;
+
+    private void Start()
+    {
+        ass = GetComponent<AudioSource>();
+    }
+
+    private void Update()
+    {
+        if(ass.isPlaying == true)
+        {
+            ass.Stop();
+        }
+        else if(ass.isPlaying == false)
+        {
+            ass.Play();
+        }
+    }
+}
