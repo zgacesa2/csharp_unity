f55938c0bd8d63911843d467cbb317f98e353035
Zoran <zoran.gacesa2@gmail.com>
2020-07-23 18:03:39 +0200

    Create Zadatak1.cs

---
+++ C#_unity_zadace/05_GameObject/Zadatci2/Zadatak1.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// U startu uništite game object na kojem je skripta
+
+public class Zadatak1 : MonoBehaviour
+{
+    private void Start()
+    {
+        //Destroy(gameObject);
+        Destroy(this.gameObject);
+    }
+}
