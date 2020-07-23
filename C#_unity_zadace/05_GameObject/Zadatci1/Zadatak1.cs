deef29b6d4e743a7f989b1360e60640aa6e0c56a
Zoran <zoran.gacesa2@gmail.com>
2020-07-23 18:06:36 +0200

    Create Zadatak1.cs

---
+++ C#_unity_zadace/05_GameObject/Zadatci1/Zadatak1.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// U startu isključite game object
+
+public class Zadatak1 : MonoBehaviour
+{
+    public GameObject predmet;
+
+    private void Start()
+    {
+        //NE: predmet = GetComponent<GameObject>();
+        //predmet = gameObject;
+        predmet = this.gameObject;
+        predmet.SetActive(true);
+    }
+}
