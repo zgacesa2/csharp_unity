9b7c08715332f43aa9d2efc94a40346ce2f601eb
Zoran <zoran.gacesa2@gmail.com>
2020-07-23 18:04:58 +0200

    Create Zadatak3.cs

---
+++ C#_unity_zadace/03_Colliders/Triggers/Zadatak3.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Napravite skriptu koja ima javnu varijablu "Health".
+// Kada cube stoji u sferi neka gubi health za "5 * Time.deltaTime"
+
+public class Zadatak3 : MonoBehaviour
+{
+    public float Health;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.tag == "Sfera")
+        {
+            Health -= 5 * Time.deltaTime;
+        }
+    }
+}
