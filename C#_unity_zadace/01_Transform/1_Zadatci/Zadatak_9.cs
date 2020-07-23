1f7766ae811d3c0b84ebed0cc8f89693ab61ed63
Zoran <zoran.gacesa2@gmail.com>
2020-07-23 17:55:09 +0200

    Create Zadatak_9.cs

---
+++ C#_unity_zadace/01_Transform/1_Zadatci/Zadatak_9.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Napravite skriptu koja može pomicati kocku po svim osima (javne varijable), 
+//rotirati (javne varijable) i povećavati (javne varijable) svake sekunde
+
+public class Zadatak_9 : MonoBehaviour
+{
+    public float xPosition;
+    public float yPosition;
+    public float zPosition;
+    public float xRotation;
+    public float yRotation;
+    public float zRotation;
+    public float xScale;
+    public float yScale;
+    public float zScale;
+
+    private void Update()
+    {
+        transform.position += new Vector3(xPosition, yPosition, zPosition) * Time.deltaTime;
+        transform.localScale += new Vector3(xScale, yScale, zScale) * Time.deltaTime;
+        transform.Rotate(Vector3.right * xRotation * Time.deltaTime);
+        transform.Rotate(Vector3.up * yRotation * Time.deltaTime);
+        transform.Rotate(Vector3.forward * zRotation * Time.deltaTime);
+    }
+}
