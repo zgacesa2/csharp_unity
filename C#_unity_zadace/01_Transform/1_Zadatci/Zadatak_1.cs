2c2e86d39b9cb4f3b0b2d1d036d3d5af28953f7e
Zoran <zoran.gacesa2@gmail.com>
2020-07-23 16:52:12 +0200

    Create Zadatak_1.cs

---
+++ C#_unity_zadace/01_Transform/1_Zadatci/Zadatak_1.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Rotirajte kocku za 1 stupanj svaki frame po x, y i z osima
+
+public class Zadatak_1 : MonoBehaviour
+{
+    private void Update()
+    {
+        transform.Rotate(Vector3.right);
+        transform.Rotate(Vector3.up);
+        transform.Rotate(Vector3.forward);
+    }
+}
