94d1342d8b58a5722fe8c6e62c348399703b7f7e
Zoran <zoran.gacesa2@gmail.com>
2020-07-23 18:01:31 +0200

    Create Zadatak4.cs

---
+++ C#_unity_zadace/01_Transform/5_Zadatci/Zadatak4.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Napišite skriptu koja će uzeti početne vrijednosti objekta na sceni 
+//(njegov scale) i invertati ga (sve scale vrijednosti postaviti na minus)
+public class zadatak_4 : MonoBehaviour
+{
+    public float xScale;
+    public float yScale;
+    public float zScale;
+    
+    void Start()
+    {
+        transform.localScale =new Vector3(xScale, yScale, zScale) * (-1);
+    }
+}
