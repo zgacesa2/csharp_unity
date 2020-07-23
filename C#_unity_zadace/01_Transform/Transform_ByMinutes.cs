e782d66d41ba9b072e04996d1209b39a5ddec590
Zoran <zoran.gacesa2@gmail.com>
2020-07-23 18:00:07 +0200

    Create Transform_ByMinutes.cs

---
+++ C#_unity_zadace/01_Transform/Transform_ByMinutes.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Transform_ByMinutes : MonoBehaviour
+{
+    public float xRotation = 5;
+
+    private void Update()
+    {
+        transform.Rotate(Vector3.right * xRotation * Time.deltaTime);
+        //DeltaTime (Time.DeltaTime) nam je izračun od framea do framea
+        //Tako dobivamo 1 sekundu u metodi Update
+    }
+}
