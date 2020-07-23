eb30426b64ca52c510c9d67bb8d67169e77e93e7
Zoran <zoran.gacesa2@gmail.com>
2020-07-23 18:00:17 +0200

    Create Transform_Scale.cs

---
+++ C#_unity_zadace/01_Transform/Transform_Scale.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Transform_Scale : MonoBehaviour
+{
+    public float xScale;
+    public float yScale;
+    public float zScale;
+
+    private void Update()
+    {
+        transform.localScale += new Vector3(xScale, yScale, zScale);
+    }
+}
