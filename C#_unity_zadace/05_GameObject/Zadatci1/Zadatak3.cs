77c6be29297810ba9e5fdd57bbfa9d94947be09a
Zoran <zoran.gacesa2@gmail.com>
2020-07-23 18:04:05 +0200

    Create Zadatak3.cs

---
+++ C#_unity_zadace/05_GameObject/Zadatci1/Zadatak3.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Na tipku "S" ugasite game object
+
+public class Zadatak3 : MonoBehaviour
+{
+    public GameObject buhtla;
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.S))
+        {
+            buhtla.SetActive(!buhtla.activeSelf);
+        }
+    }
+}
