9ad14ab80bdc302d8d342dbfdaa56d779061f7e0
Zoran <zoran.gacesa2@gmail.com>
2020-07-23 18:05:30 +0200

    Create Papirici2.cs

---
+++ C#_unity_zadace/04_AudioSource/Papirici2.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Promijeni glasnocu audio sourca prema javnoj varijabli
+
+public class Papirici2 : MonoBehaviour
+{
+    public float vol;
+    AudioSource source;
+
+    private void Start()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    private void Update()
+    {
+        source.volume = vol;
+    }
+}
