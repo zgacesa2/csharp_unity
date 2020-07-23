681dd1cd68d23a37d26519dc96078b32c64167e7
Zoran <zoran.gacesa2@gmail.com>
2020-07-23 16:52:21 +0200

    Create Zadatak_2.cs

---
+++ C#_unity_zadace/01_Transform/1_Zadatci/Zadatak_2.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Rotirajte kocku za 1 stupanj svake sekunde po x, y i z osima
+
+public class Zadatak_2 : MonoBehaviour
+{
+    private void Update()
+    {
+        transform.Rotate(new Vector3(1, 1, 1) * Time.deltaTime);
+    }
+}
