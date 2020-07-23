4b538a4c89fd1a926dd1904081caf277314af86c
Zoran <zoran.gacesa2@gmail.com>
2020-07-23 18:03:46 +0200

    Create Zadatak2.cs

---
+++ C#_unity_zadace/05_GameObject/Zadatci2/Zadatak2.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Na tipku S uništite ovu skriptu sa objekta
+
+public class Zadatak2 : MonoBehaviour
+{
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.S))
+        {
+            Destroy(this);
+        }
+    }
+}
