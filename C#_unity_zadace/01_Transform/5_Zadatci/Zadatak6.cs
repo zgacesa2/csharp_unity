df717bc5cb8ecdc99254fb3a96b7f92a598411de
Zoran <zoran.gacesa2@gmail.com>
2020-07-23 18:01:48 +0200

    Create Zadatak6.cs

---
+++ C#_unity_zadace/01_Transform/5_Zadatci/Zadatak6.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dz5_6: MonoBehaviour
+{
+    public float yzScale;
+
+private void Start()
+{
+    for (int i = 0; i <= 10, i++) ;
+    {
+        transform.localScale -= newVector3(yzScale, yzScale, yzScale);
+    }
+
+}
+}
