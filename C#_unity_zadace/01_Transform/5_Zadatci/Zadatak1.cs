fcea3eb43073c675795e7368d42aed360b017291
Zoran <zoran.gacesa2@gmail.com>
2020-07-23 18:01:23 +0200

    Create Zadatak1.cs

---
+++ C#_unity_zadace/01_Transform/5_Zadatci/Zadatak1.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// DZ_POJEDINACNO
+// 1) Napišite koju vrijednost u C# jeziku imaju sljedeći izrazi:
+
+public class Borna_Zadatak_5_1 : MonoBehaviour
+{
+    public void Start()
+    {
+        // a)
+        Debug.Log(2 + 8 * 3); // 26
+
+        // b)
+        Debug.Log(8 * (3 + 2)); // 40
+
+        // c)
+        Debug.Log(8 / 3 + 2); // 4
+
+        // d)
+        Debug.Log(8 % 3 + 2); // 4
+
+        // e)
+        Debug.Log(31 % 4 + 9 * 2); // 3 + 18 = 21
+
+        // f)
+        Debug.Log(9 * (2) + 2); // 18 + 2 = 20
+
+        // g)
+        //Debug.Log(9 / (4 >= 2 + 2)); //COMPILE TIME ERROR -> int with bool -> 9 je int, 4 <= 2 je bool
+
+        // h)
+        //Debug.Log(9 * !3 + 2); // Compile Time Error -> ! can't be used with int
+    }
+    
+}
