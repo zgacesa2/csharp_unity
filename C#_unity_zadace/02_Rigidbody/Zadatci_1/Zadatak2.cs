97b9ad119f4a8cc6d1445e030109a0f304a2c2a0
Zoran <zoran.gacesa2@gmail.com>
2020-07-23 18:02:19 +0200

    Create Zadatak2.cs

---
+++ C#_unity_zadace/02_Rigidbody/Zadatci_1/Zadatak2.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Na klik space upaliti ili ugasiti gravitaciju na objektu
+
+public class Zadatak2 : MonoBehaviour
+{
+    Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            if(rb.useGravity) //isto kao da sam napisao (rb.useGravity == true)
+            {
+                rb.useGravity = false;
+            }
+            else if(!rb.useGravity)//isto kao da sam napisao (rb.useGravity == false)
+            {
+                rb.useGravity = true;
+            }
+        }
+    }
+}
