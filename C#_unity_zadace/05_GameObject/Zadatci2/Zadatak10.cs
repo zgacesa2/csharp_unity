9a2b3d98d8a149e0c2bc9398fb2ae012fc613909
Zoran <zoran.gacesa2@gmail.com>
2020-07-23 18:03:42 +0200

    Create Zadatak10.cs

---
+++ C#_unity_zadace/05_GameObject/Zadatci2/Zadatak10.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Svakih 5 sekudni neka se u debug.logu ispiše "ja sam magarac"
+
+public class Zadatak10 : MonoBehaviour
+{
+    public float timer = 5;
+
+    private void Update()
+    {
+        timer -= Time.deltaTime;
+        if(timer <= 0)
+        {
+            Debug.Log("Ja sam magarac");
+            timer = 5;
+        }
+    }
+}
