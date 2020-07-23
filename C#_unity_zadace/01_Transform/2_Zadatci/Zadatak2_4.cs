381584158feaabea3363e0b3c8b8f54a3be181ea
Zoran <zoran.gacesa2@gmail.com>
2020-07-23 17:59:50 +0200

    Create Zadatak2_4.cs

---
+++ C#_unity_zadace/01_Transform/2_Zadatci/Zadatak2_4.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*Rotirajte kocku za 3 stupnja svaku sekundu:
+a) Po X osi
+b) po Y osi
+c) Po X i Y osi
+D) Kada se rotira po X i Y osi, rotira li se i po Z? - DA
+*/
+
+public class Zadatak2_4 : MonoBehaviour
+{
+    private void Update()
+    {
+        //C: oboje a + b
+        //a
+        transform.Rotate(new Vector3(3, 0, 0) * Time.deltaTime);
+        //b
+        transform.Rotate(new Vector3(0, 3, 0) * Time.deltaTime);
+    }
+}
