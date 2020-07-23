671a9e2905cc1d24062ad2bc4c1b6691bc7ba4f4
Zoran <zoran.gacesa2@gmail.com>
2020-07-23 18:03:13 +0200

    Create TriggerEnter.cs

---
+++ C#_unity_zadace/03_Colliders/Triggers/TriggerEnter.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//KAKO BI RADIO ON TRIGGER BAREM JEDAN OD OBJEKATA MORA IMATI RIGIDBODY
+// MORA BITI UKLJUČEN ISTRIGGER NA OBJEKTU
+
+public class TriggerEnter : MonoBehaviour
+{
+    public float Health = 100;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Ušao je");
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Vrata")
+        {
+            Health += 10 * Time.deltaTime;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Vrata")
+        {
+            Debug.Log("Izašao je");
+        }
+    }
+}
