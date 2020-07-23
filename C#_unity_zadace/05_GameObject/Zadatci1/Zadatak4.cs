7dd9ef7fd7f35287810990f828bd13362da158cc
Zoran <zoran.gacesa2@gmail.com>
2020-07-23 18:03:35 +0200

    Create Zadatak4.cs

---
+++ C#_unity_zadace/05_GameObject/Zadatci1/Zadatak4.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Kada kocka dotakne sferu, neka se sfera ugasi
+
+public class Zadatak4 : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        other.gameObject.SetActive(false);
+    }
+}
