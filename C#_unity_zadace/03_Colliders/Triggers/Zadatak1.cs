aa939353a597c38ff13a2979abd0533a4ee00078
Zoran <zoran.gacesa2@gmail.com>
2020-07-23 18:03:20 +0200

    Create Zadatak1.cs

---
+++ C#_unity_zadace/03_Colliders/Triggers/Zadatak1.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* Napišite skriptu koja ispisuje u debugu kada je cube ušao u sferu, 
+ * kada je unutra i kada je izašao.
+ */
+
+public class Zadatak1 : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Ušao je");
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        Debug.Log("Unutra je");
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log("Izašao je van");
+    }
+}
