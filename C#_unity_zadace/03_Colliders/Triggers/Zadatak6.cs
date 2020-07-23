bc03ca1ec7b2c0b634009c401beda9fbef4eb809
Zoran <zoran.gacesa2@gmail.com>
2020-07-23 18:05:06 +0200

    Create Zadatak6.cs

---
+++ C#_unity_zadace/03_Colliders/Triggers/Zadatak6.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* 
+ * Napravite jos jednu kocku i kopirajte ih da ih imate 10 na sceni.
+ * Imajte javnu varijablu "score", kada god uđete u jednu od tih kocka
+ * neka vam se score poveća za jedan.
+ */
+
+public class Zadatak6 : MonoBehaviour
+{
+    public int score;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag=="score")
+        {
+            score += 1;
+        }
+    }
+}
