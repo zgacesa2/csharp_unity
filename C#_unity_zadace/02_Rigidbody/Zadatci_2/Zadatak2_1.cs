7f6d19d149ddf9b93a771d40fd4db8d7117f524d
Zoran <zoran.gacesa2@gmail.com>
2020-07-23 18:02:50 +0200

    Create Zadatak2_1.cs

---
+++ C#_unity_zadace/02_Rigidbody/Zadatci_2/Zadatak2_1.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Učitajte rigidbody bez povlačenja
+
+public class Zadatak2_1 : MonoBehaviour
+{
+    Rigidbody rb;    
+    private void Start()
+    {
+        rb = transform.GetComponent<Rigidbody>();
+    }
+}
