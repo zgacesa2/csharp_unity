e86da59d9bbb52f657e66e1461154dafa6a7e8b0
Zoran <zoran.gacesa2@gmail.com>
2020-07-23 18:00:10 +0200

    Create Transform_Position.cs

---
+++ C#_unity_zadace/01_Transform/Transform_Position.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Transform_Position : MonoBehaviour
+{
+    public float xSpeed;
+    public float ySpeed;
+    public float zSpeed;
+
+    private void Update()
+    {
+        transform.position += new Vector3(xSpeed, ySpeed, zSpeed);
+    }
+}
