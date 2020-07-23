f6b19d54637c8123065554e71decf4b9eea45e31
Zoran <zoran.gacesa2@gmail.com>
2020-07-23 18:00:40 +0200

    Create Zadatak3_2.cs

---
+++ C#_unity_zadace/01_Transform/3_Zadatci/Zadatak3_2.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Napišite skriptu koja kocku povećava ili smanjuje za javne vrijednosti u beskonaćnost
+
+public class Zadatak3_2 : MonoBehaviour
+{
+    public float XScale;
+    public float YScale;
+    public float ZScale;
+    public bool increase = false;
+
+    private void Update()
+    {
+        transform.localScale += new Vector3(XScale, YScale, ZScale);
+        if (increase == true)
+        {
+            transform.localScale += new Vector3(XScale, YScale, ZScale) * Time.deltaTime;
+        }
+        else
+        {
+            transform.localScale -= new Vector3(XScale, YScale, ZScale) * Time.deltaTime;
+        }
+    }
+}
