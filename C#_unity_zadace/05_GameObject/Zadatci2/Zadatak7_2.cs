b13f7e71edcc342c9c5e0c375eab41cf85430700
Zoran <zoran.gacesa2@gmail.com>
2020-07-23 18:04:01 +0200

    Create Zadatak7_2.cs

---
+++ C#_unity_zadace/05_GameObject/Zadatci2/Zadatak7_2.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// U startu neka se stvori na sceni na poziciji 0,0,0
+// objekt kocke koji je spremljen prije toga kao prefab
+
+public class Zadatak7_2 : MonoBehaviour
+{
+    public GameObject nasPrefab;
+    public Transform spawnPosition;
+
+    private void Start()
+    {
+        Instantiate(nasPrefab, spawnPosition.position, spawnPosition.rotation);
+    }
+}
