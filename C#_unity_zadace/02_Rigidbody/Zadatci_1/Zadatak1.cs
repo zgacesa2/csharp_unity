ff8bb5e7ccdb7e550d0db47467f9109b0db679b6
Zoran <zoran.gacesa2@gmail.com>
2020-07-23 18:00:23 +0200

    Create Zadatak1.cs

---
+++ C#_unity_zadace/02_Rigidbody/Zadatci_1/Zadatak1.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Dodjeliti rigidbody na skriptu
+
+public class Zadatak1 : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+}
