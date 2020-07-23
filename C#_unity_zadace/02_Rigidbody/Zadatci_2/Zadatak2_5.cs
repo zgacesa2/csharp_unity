e713f97982e31421c63a0500fea5afc25fcf0550
Zoran <zoran.gacesa2@gmail.com>
2020-07-23 18:03:05 +0200

    Create Zadatak2_5.cs

---
+++ C#_unity_zadace/02_Rigidbody/Zadatci_2/Zadatak2_5.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Napravite skriptu koja na tipku W udara silom na objekt u smjeru X osi,
+// na tipku S u negativnom smjeru X osi, na tipku A u pozitivnom smjeru Z osi,
+// a na tipku D u negativnom smjeru Z osi
+
+public class Zadatak2_5 : MonoBehaviour
+{
+    Rigidbody rb;
+    public float speed = 3.5f;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb.AddForce(transform.right * speed);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            rb.AddForce(transform.right * -speed);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.AddForce(transform.forward * speed);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.AddForce(transform.forward * -speed);
+        }
+    }
+}
