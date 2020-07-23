859a05d7b81d851adbdf1c5ddc0b8934be30e59d
Zoran <zoran.gacesa2@gmail.com>
2020-07-23 18:00:36 +0200

    Create Zadatak2_5.cs

---
+++ C#_unity_zadace/01_Transform/2_Zadatci/Zadatak2_5.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* Scejlajte kocku za:
+a) 0.25 po Y osi svaki frame
+b) 89 po Z osi pri pokretanju igre
+c) Kada se odradio b) zadatak neka debug ispiše kolika je trenutna veličina Z osi
+*/
+
+public class Zadatak2_5 : MonoBehaviour
+{
+    private void Start()
+    {
+        //b
+        transform.localScale += new Vector3(0, 0, 89);
+        //c
+        Debug.Log("Trenutna veličina z osi je " + transform.localScale.z);
+    }
+    private void Update()
+    {
+        //a
+        transform.localScale += new Vector3(0, 0.25f, 0);
+    }
+}
