bd5c902a50197469204891a8fea4a9f2c9190737
Zoran <zoran.gacesa2@gmail.com>
2020-07-23 18:02:35 +0200

    Create Zadatak3.cs

---
+++ C#_unity_zadace/02_Rigidbody/Zadatci_1/Zadatak3.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Skripta koja objekt kreće po x osi uz pomoć sile
+
+public class Zadatak3 : MonoBehaviour
+{
+    public float brzina;
+    Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    //Kada radimo sa silama koristimo FixedUdate, a ne obicni update zbog optimizacije
+    private void FixedUpdate()
+    {
+        rb.AddForce(transform.right * brzina);
+    }
+}
