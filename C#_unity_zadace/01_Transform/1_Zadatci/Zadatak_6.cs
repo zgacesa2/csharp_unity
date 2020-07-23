8ad608d747860cc7bd7b3af46b7b8829596112cb
Zoran <zoran.gacesa2@gmail.com>
2020-07-23 17:54:57 +0200

    Create Zadatak_6.cs

---
+++ C#_unity_zadace/01_Transform/1_Zadatci/Zadatak_6.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Povećajte kocku za 3 u samom pokretanju igre po Y osi
+
+public class Zadatak_6 : MonoBehaviour
+{
+    private void Start()
+    {
+        transform.localScale += Vector3.up * 3;
+    }
+}
