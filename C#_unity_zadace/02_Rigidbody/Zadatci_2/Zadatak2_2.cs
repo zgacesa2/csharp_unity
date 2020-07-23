c8b95f0d9caaccb4b6cd990c7f3ab6f67c4a8614
Zoran <zoran.gacesa2@gmail.com>
2020-07-23 18:02:54 +0200

    Create Zadatak2_2.cs

---
+++ C#_unity_zadace/02_Rigidbody/Zadatci_2/Zadatak2_2.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// U skripti definirajte janvu varijablu "mass". 
+// Rigidbodyu povećajte težinu NA vrijednost javne varijable
+
+public class Zadatak2_2 : MonoBehaviour
+{
+    public float masa = 3;
+    Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.mass = masa;
+    }
+}
