803232c443e4fb3b6e29b6d6fd195b465e89e8c8
Zoran <zoran.gacesa2@gmail.com>
2020-07-23 18:02:07 +0200

    Create Transform_All.cs

---
+++ C#_unity_zadace/01_Transform/Transform_All.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//1. Zadatak
+// Rotirajte kocku za 1 stupanj svaki frame
+//a.) Po X osi
+//b.) Po Y osi
+//c.) Po Z osi
+public class Transform_All : MonoBehaviour
+{
+    public float speedX;
+    public float speedY;
+    public float speedZ;
+
+    private void Update()
+    {
+        //a.)
+        transform.Rotate(Vector3.right * speedX);
+        //b.)
+        transform.Rotate(Vector3.up * speedY);
+        //c.)
+        transform.Rotate(Vector3.forward * speedZ);
+    }
+}
