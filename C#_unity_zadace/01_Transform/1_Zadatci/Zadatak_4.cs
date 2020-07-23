0f654c1143de955aa613cd1321edeef98862a5c1
Zoran <zoran.gacesa2@gmail.com>
2020-07-23 16:52:50 +0200

    Create Zadatak_4.cs

---
+++ C#_unity_zadace/01_Transform/1_Zadatci/Zadatak_4.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Povećajte kocku za 0.1 svaki frame
+
+public class Zadatak_4 : MonoBehaviour
+{
+    private void Update()
+    {
+        transform.localScale += new Vector3(0.1f, 0.1f, 0.1f); 
+    }
+}
