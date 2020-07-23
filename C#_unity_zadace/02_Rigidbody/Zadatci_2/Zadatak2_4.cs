75410e7ad110c1ab7023b3744894689f51aeac08
Zoran <zoran.gacesa2@gmail.com>
2020-07-23 18:03:01 +0200

    Create Zadatak2_4.cs

---
+++ C#_unity_zadace/02_Rigidbody/Zadatci_2/Zadatak2_4.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// U start metodu napravite var vrstu varijable gdje pristupate 
+// isključivo gravitaciji rigidbodya te ju isključite
+
+public class Zadatak2_4 : MonoBehaviour
+{
+     void Start()
+    {
+        var gravity = GetComponent<Rigidbody>().useGravity;
+        gravity = false;
+    }
+}
