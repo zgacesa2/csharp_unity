60f76c1b734d01ab4f9fb6b7737f8082f532bb2f
Zoran <zoran.gacesa2@gmail.com>
2020-07-23 21:59:20 +0200

    Create Zadatak8.cs

---
+++ C#_unity_zadace/05_GameObject/Zadatci1/Zadatak8.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zadatak8 : MonoBehaviour
+{
+    //Napravite skriptu koja svaki frame pali/gasi objekt. Na tipku F skripta to prestaje raditi. 
+    //Kada se stisne opet tipka F skripta opet o počinje raditi (obavezno koristiti vlastitu metodu)
+    public GameObject predmet;
+    private bool istina = true;
+
+    private void Update()
+    {
+        Tipka();
+        if(istina)
+        {
+            Pali_gasi();
+        }
+    }
+    private void Tipka()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            istina = !istina;
+        }
+    }
+    void Pali_gasi()
+    {
+       predmet.SetActive(!predmet.activeSelf);
+    }
+}
