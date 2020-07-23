894596092a0eb6f41c267158cd69be2c344b3494
Zoran <zoran.gacesa2@gmail.com>
2020-07-23 16:53:36 +0200

    Create Zadatak_5.cs

---
+++ C#_unity_zadace/01_Transform/1_Zadatci/Zadatak_5.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Povećajte kocku za javne vrijednosti za sve tri osi i povećavajte ju 
+//za te vrijednosti po osima svaku sekundu
+
+public class Zadatak_5 : MonoBehaviour
+{
+    public float xScale;
+    public float yScale;
+    public float zScale;
+
+    private void Update()
+    {
+        transform.localScale += new Vector3(xScale, yScale, zScale) * Time.deltaTime;
+    }
+}
