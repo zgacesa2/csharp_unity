a182b16f8a2dde893774c8eb4ba3d23e9bb5f637
Zoran <zoran.gacesa2@gmail.com>
2020-07-23 18:06:18 +0200

    Create Zadatak1.cs

---
+++ C#_unity_zadace/04_AudioSource/Zadatci1/Zadatak1.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Napravite skriptu gdje u startu pokrećete AudioSource
+
+public class Zadatak1 : MonoBehaviour
+{
+    AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.Play();
+    }
+}
