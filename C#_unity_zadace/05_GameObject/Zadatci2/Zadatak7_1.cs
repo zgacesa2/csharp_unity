3c923d944c16e0f8c0128d12cc28f4ddec78f76c
Zoran <zoran.gacesa2@gmail.com>
2020-07-23 18:03:57 +0200

    Create Zadatak7_1.cs

---
+++ C#_unity_zadace/05_GameObject/Zadatci2/Zadatak7_1.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// U startu neka se stvori na sceni na poziciji 0,0,0
+// objekt kocke koji je spremljen prije toga kao prefab
+
+public class Zadatak7_1 : MonoBehaviour
+{
+    public GameObject prefabNas; //Iz projecta iz flodera prefabs
+
+    private void Start()
+    {
+        Instantiate(prefabNas, new Vector3(0, 0, 0), Quaternion.identity);
+    }
+}
