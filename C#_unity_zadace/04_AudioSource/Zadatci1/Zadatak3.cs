c231f0b7708894e419285ca01de93a03bb285226
Zoran <zoran.gacesa2@gmail.com>
2020-07-23 18:06:23 +0200

    Create Zadatak3.cs

---
+++ C#_unity_zadace/04_AudioSource/Zadatci1/Zadatak3.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Na tipku M neka se zvuk mutea, a na tipku K neka se unmutea.
+
+public class Zadatak3 : MonoBehaviour
+{
+    AudioSource audioS;
+
+    private void Start()
+    {
+        audioS = GetComponent<AudioSource>();
+    }
+
+    private void Update()
+    {
+        if(audioS.mute == true && Input.GetKeyDown(KeyCode.K))
+        {
+            audioS.mute = false;
+        }
+        if (audioS.mute == false && Input.GetKeyDown(KeyCode.M))
+        {
+            audioS.mute = true;
+        }
+    }
+}
