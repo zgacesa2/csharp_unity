b0ffad115de8a4b72e648aba684b62bedf83e011
Zoran <zoran.gacesa2@gmail.com>
2020-07-23 18:00:54 +0200

    Create Zadatak1.cs

---
+++ C#_unity_zadace/01_Transform/4_Zadatci/Zadatak1.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ante_DZ : MonoBehaviour
+{
+    //1) Unesite dvije javne vrijednosti.
+    //Neka se objekt na sceni uveća po svim osima u početku za zbroj te dvije javne vrijednosti.
+
+    public int a;
+    public int b;
+
+    private void Start()
+    {
+        a = 55;
+        b = 60;
+
+        transform.localScale += new Vector3(a + b, a + b, a + b);
+    }
+
+}
