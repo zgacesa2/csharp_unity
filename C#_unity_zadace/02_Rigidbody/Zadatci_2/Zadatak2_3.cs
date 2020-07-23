2209ec2e9efcf9685c99368dd3860f4b9d01d511
Zoran <zoran.gacesa2@gmail.com>
2020-07-23 18:02:58 +0200

    Create Zadatak2_3.cs

---
+++ C#_unity_zadace/02_Rigidbody/Zadatci_2/Zadatak2_3.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Rigidboyu na pritisak tipke "K" ukljucite ili iskljucite kinematiku,
+// a na klik g uključite ili isključite gravitaciju
+
+public class Zadatak2_3 : MonoBehaviour
+{
+    Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            rb.isKinematic = !rb.isKinematic;
+        }
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            rb.useGravity = !rb.useGravity;
+        }
+    }
+}
