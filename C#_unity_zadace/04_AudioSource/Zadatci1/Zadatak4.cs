1b117a68b78a156e81ea89942514c8c7ef7dac55
Zoran <zoran.gacesa2@gmail.com>
2020-07-23 18:06:27 +0200

    Create Zadatak4.cs

---
+++ C#_unity_zadace/04_AudioSource/Zadatci1/Zadatak4.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Mute i unmute svaki frame
+
+public class Zadatak4 : MonoBehaviour
+{
+    AudioSource aSource;
+
+    private void Start()
+    {
+        aSource = GetComponent<AudioSource>();
+    }
+
+    private void Update()
+    {
+        if(aSource.mute == false)
+        {
+            aSource.mute = true;
+        }
+        else if(aSource.mute == true)
+        {
+            aSource.mute = false;
+        }
+    }
+}
