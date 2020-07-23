c2084ab7ef0a0d9fb19de0a838ed15128e97da1f
Zoran <zoran.gacesa2@gmail.com>
2020-07-23 17:55:13 +0200

    Create Zadatak2_3.cs

---
+++ C#_unity_zadace/01_Transform/2_Zadatci/Zadatak2_3.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Napišite skriptu koja će imati Debug.Log() na:
+//a) Awake
+//b) Start
+//c) Update
+
+public class Zadatak2_3 : MonoBehaviour
+{
+    private void Awake()
+    {
+        Debug.Log("Ovo je Awake");
+    }
+
+    private void Start()
+    {
+        Debug.Log("Ovo je Start");
+    }
+
+    private void Update()
+    {
+        Debug.Log("Ovo je update");
+    }
+}
