9f38d40e488fb83ae39a08051878003bf5514f02
Zoran <zoran.gacesa2@gmail.com>
2020-07-23 18:01:14 +0200

    Create Zadatak6.cs

---
+++ C#_unity_zadace/01_Transform/4_Zadatci/Zadatak6.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ROTIRANJE : MonoBehaviour
+{
+    //Učini da se kocka rotira po svim osima u isto vrijeme na najkraći način.
+    private void Update()
+    {
+        transform.Rotate(Vector3.one);
+
+    }
+}
