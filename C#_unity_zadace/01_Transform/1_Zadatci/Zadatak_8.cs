15384aa832b8073f444a6f771be1f959cc75fb8c
Zoran <zoran.gacesa2@gmail.com>
2020-07-23 17:59:40 +0200

    Create Zadatak_8.cs

---
+++ C#_unity_zadace/01_Transform/1_Zadatci/Zadatak_8.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Neka se sfera pozicionira na poziciju 0, 12, 3.75 u početku igre
+
+public class Zadatak_8 : MonoBehaviour
+{
+    float xPosition = 0f;
+    float yPosition = 12f;
+    float zPosition = 3.75f;
+
+    private void Start()
+    {
+        transform.position = new Vector3(xPosition, yPosition, zPosition);
+    }
+}
