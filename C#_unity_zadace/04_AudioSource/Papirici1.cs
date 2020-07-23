8fd5179fbf7f3ec466eb49507ec6c6890ddaa264
Zoran <zoran.gacesa2@gmail.com>
2020-07-23 18:05:26 +0200

    Create Papirici1.cs

---
+++ C#_unity_zadace/04_AudioSource/Papirici1.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Napiši kako pozivamo metodu za nalaženje Transform komponente objekta koji se zove keks
+
+public class Papirici1 : MonoBehaviour
+{
+    //ova skripta se nalazi na objektu koji se zove keks
+    public Transform keksTransform;
+
+    private void Start()
+    {
+        keksTransform = GetComponent<Transform>();
+    }
+}
