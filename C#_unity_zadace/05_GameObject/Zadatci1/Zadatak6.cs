41d426e5c1c07f1579214266974d168ab95724be
Zoran <zoran.gacesa2@gmail.com>
2020-07-23 18:03:27 +0200

    Create Zadatak6.cs

---
+++ C#_unity_zadace/05_GameObject/Zadatci1/Zadatak6.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//6.) Napravite kocku koja na sebi ima audiosource koji je aktivan od starta i svira. 
+//Napravite skriptu s kojom kasite ili palite objekt na kojem je audio source (tipka po vašem izboru). 
+//Što se događa kada se ugasi objekt, cuje li se i dalje zvuk?
+public class Zadatak6 : MonoBehaviour
+{
+    public GameObject predmet;
+    AudioSource zvuk;
+
+    private void Start()
+    {
+        zvuk = GetComponent<AudioSource>();
+        zvuk.Play();
+    }
+
+    private void Update()
+    {
+        if (predmet.activeSelf == true && Input.GetKeyDown(KeyCode.S))
+        {
+            predmet.SetActive(false);
+        }
+        else if (predmet.activeSelf == false && Input.GetKeyDown(KeyCode.S))
+        {
+            predmet.SetActive(true);
+        }
+    }
+}
