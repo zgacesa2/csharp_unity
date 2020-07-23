b10a569121056ba19673a42527158e383af09aa8
Zoran <zoran.gacesa2@gmail.com>
2020-07-23 18:02:03 +0200

    Create DebugZadatak.cs

---
+++ C#_unity_zadace/01_Transform/DebugZadatak.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugZadatak : MonoBehaviour
+{
+    private void Start()
+    {
+        var pon = new Vector3(1, 2, 3) * 3.1f;
+        var uto = new Vector3(12, 21, 11) * 5.22f;
+        var sri = pon + uto - pon + pon;
+        var cet = "pobogu" + uto + uto;
+        var pet = cet + "bas" + sri;
+        var sub = "ja" + sri + cet + pet;
+
+        Debug.Log(sub);
+        //ja(65.7, 115.8, 66.7)pobogu(62.6, 109.6, 57.4)(62.6, 109.6, 57.4)pobogu(62.6, 109.6, 57.4)pobogu(62.6, 109.6, 57.4)(62.6, 109.6, 57.4)bas(65.7, 115.8, 66.7)
+    }
+}
