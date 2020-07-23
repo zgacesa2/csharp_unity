7209affc5de0cc233296b203f7d0769da5ce3817
Zoran <zoran.gacesa2@gmail.com>
2020-07-23 18:01:02 +0200

    Create Zadatak3.cs

---
+++ C#_unity_zadace/01_Transform/4_Zadatci/Zadatak3.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DomacaZadaca : MonoBehaviour
+{
+
+    public int x;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (x<=100)
+        {            
+            transform.localScale += Vector3.right * x;
+            x++;
+        }
+    }
+}
