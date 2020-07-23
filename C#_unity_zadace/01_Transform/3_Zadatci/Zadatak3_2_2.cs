16f353a8afabae967446dec5a988d635805ab34a
Zoran <zoran.gacesa2@gmail.com>
2020-07-23 18:00:50 +0200

    Create Zadatak3_2_2.cs

---
+++ C#_unity_zadace/01_Transform/3_Zadatci/Zadatak3_2_2.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zadatak3_2_2 : MonoBehaviour
+{
+    public float xScale;
+    public float yScale;
+    public float zScale;
+
+    private void Update()
+    {
+        transform.localScale += new Vector3(xScale, yScale, zScale) * Time.deltaTime;
+    }
+}
