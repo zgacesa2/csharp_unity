7d325af4d20d0211aefb45baff688438927e5ea2
Zoran <zoran.gacesa2@gmail.com>
2020-07-23 18:00:13 +0200

    Create JumpScript.cs

---
+++ C#_unity_zadace/02_Rigidbody/JumpScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// napiši skriptu za Jump
+
+public class JumpScript : MonoBehaviour
+{
+    Rigidbody rb;
+    public float jump;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    private void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(transform.up * jump);
+        }
+    }
+}
